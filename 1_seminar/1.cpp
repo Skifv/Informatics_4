deaa43975f43c7d08e1569b2e5e68f4a7322b6b5
Skif <lepin.vd@phystech.edu>
2025-02-22 19:08:00 +0300

    add 1-10

--- 1_seminar/1.cpp
+++ 1_seminar/1.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Enter 2 numbers:\n";
     int a, b;
     std::cin >> a >> b;
-    std::cout << a << " + " << b << " = " << a + b << "\n";
+    std::cout << a + b << "\n";
     return 0;
-
-    
-    
 }
 
